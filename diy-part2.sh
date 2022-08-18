a94dc5b30c40d6ae998ae99c4d65d11d49d7b6c2
iglobal-developer <69662383+iglobal-developer@users.noreply.github.com>
2022-08-18 18:11:29 +0700

    Added packages

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
+# git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
+git clone https://github.com/iglobal-developer/luci-app-multi-user.git package/luci-app-multi-user
